d3c7002d5aeb1d9eaadd30222ec5c2ac06db7680
Joaquín Acevedo <joaquin@ideatranslations.com>
2024-05-25 21:09:38 -0300

    fix: Replace RepositoryRestConfigurerAdapter due it is deprecated

--- src/main/java/com/aninfo/repository/RepositoryConfig.java
+++ src/main/java/com/aninfo/repository/RepositoryConfig.java
@@ -3,10 +3,10 @@ package com.aninfo.repository;
 import com.aninfo.model.Account;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
-import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 
 @Configuration
-public class RepositoryConfig extends RepositoryRestConfigurerAdapter {
+public class RepositoryConfig implements RepositoryRestConfigurer {
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
         config.exposeIdsFor(Account.class);
